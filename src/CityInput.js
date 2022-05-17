65dfe25c476d0a140628da73469bdd13586f7720
invacctvni <200477892@student.georgianc.on.ca>
2022-05-17 16:54:21 -0400

    add search city and desctruct the unsplash url, also fetch the url.

--- src/CityInput.js
+++ src/CityInput.js
@@ -1,29 +1,63 @@
 // // component
 import {useState} from "react";
+import {AccessKey, BasicUrl, DefaultCity} from "./consts";
+import axios from "axios";
 
-const CityInput = ()=> {
-    // Use hook to save state
-    const [city,setCity] = useState('Toronto')
+const CityInput = () => {
+    // Use hook to save state (with params)
+    const [city, setCity] = useState(DefaultCity)
+    const [images, setImages] = useState([])
     const cbInput = evt => {
         let newCity = evt.target.value.trim().toLowerCase()
-
         evt.key === 'Enter' &&
-        (()=>{
-            // target get events
-            let searchCity = evt.target.value.trim().toLowerCase()
-            console.log("searchCity Result is ==>",searchCity)
-            fetchCity
-        })()
+        //make sure new city is different from old city to avoid CPU consumption.
+        newCity !== city &&
+        (() => {
+            // console.log("searchCity Result is ==>", searchCity)
+            setCity(newCity) //async function (may not be updated immediately).
+            console.log('new city is===>', city, newCity)
+            fetchCity()
+
+        })() //() force the function to execute.
     }
+
+    const fetchCity = () => {
+        //axios -3rd part lib. google it pls.
+        //m is a method, p is a property oo.
+        axios.get(BasicUrl, {
+            params: {
+                query: city,
+                orientation: "landscape",
+            },
+            headers: {
+                Authorization: `Client-ID ${AccessKey}`
+            }
+        }).then(res => {
+            console.log("raw data", res)
+            //destruction using object.
+            let {data: {results}} = res
+            console.log('results--->',results)
+            // add () to return object
+            let imageList = results.map(item => ({
+                des: item.alt_description,
+                regular: item.urls.regular,
+                thumb: item.urls.thumb
+            }))
+            let {data:{total}} = res
+            console.log("total is ", total)
+            console.log("image list is ", imageList)
+            setImages(imageList)
+        }).catch(err => console.log('fetch city http error!', err))
+    }
+
     return <div>
-        <input type="text"onKeyDown={cbInput}/>
+        <input type="text" onKeyDown={cbInput}/>
+        {/*convert shu zu to string*/}
+        {JSON.stringify(images)}
     </div>
 }
 
 export default CityInput;
-
-
-
 
 
 // const CityInput = () => {
--- src/consts.js
+++ src/consts.js
@@ -1,1 +1,6 @@
-// test
+// all default values stored here.
+// 1. export and import must be paired.
+export const DefaultCity = 'Toronto'.toLowerCase()
+export const BasicUrl = "https://api.unsplash.com/search/photos"
+export const AccessKey = "RaSnBiqog6SVIdgaUHc2Ls-8McGCIPt7S3lsW7M3cgU"
+
